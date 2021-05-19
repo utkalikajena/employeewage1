eb505f47fda21b11fd640e84574e2e7464f52445
utkalikajena <utkalikajena97@gmail.com>
2021-05-19 17:41:30 +0530

    added welcome messege

---
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import java.net.StandardSocketOptions;
+
+class EmpWageBuilder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation program");
+    }
+}
