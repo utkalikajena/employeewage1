7f3cac042caa6e7d3520c2ab7ea42d0246d57915
utkalikajena <utkalikajena97@gmail.com>
2021-05-20 18:36:45 +0530

    ADDED:funtionality to find wage for part time

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -3,16 +3,18 @@ package com.bridgelabz;
 import java.net.StandardSocketOptions;
 
 class EmpWageBuilder {
-    private static final int IS_PRESENT = 1;
-    private static final int IS_ABSENT = 0;
+    private static final int IS_FULL_TIME = 1;
+    private static final int WAGE_PER_HR = 20;
+    private static final int  IS_PART_TIME = 2;
     public static void main(String[] args){
-        final int IS_PRESENT = 1;
-        final int WAGE_PER_HR = 20;
         int empHR = 0;
-        double empCheck = Math.floor((Math.random() * 10)) % 2;
-        System.out.println(IS_PRESENT);
-        if(empCheck == IS_PRESENT) {
+        double empCheck = Math.floor((Math.random() * 10)) % 3;
+        System.out.println(IS_FULL_TIME);
+        if(empCheck == IS_FULL_TIME) {
             empHR = 8;
+        }
+        if(empCheck == IS_PART_TIME){
+            empHR = 4;
         }
         int empWage = WAGE_PER_HR * empHR;
         System.out.println("Employee wage: "+empWage);
