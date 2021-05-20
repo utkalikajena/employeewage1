45c1791fce79021476ed8f98ce1e4e1479dc0ee4
utkalikajena <utkalikajena97@gmail.com>
2021-05-20 21:12:36 +0530

    ADDED: calculating wages for a month or 20 days

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -3,15 +3,17 @@ package com.bridgelabz;
 import java.net.StandardSocketOptions;
 
 class EmpWageBuilder {
-    private static final int IS_FULL_TIME = 2;
-    private static final int IS_PART_TIME = 1;
-    private static final int EMP_RATE_PER_HOUR = 20;
-    private static final int NUM_OF_WORKING_DAYS = 2;
+    private static final int IS_FULL_TIME = 1;
+    private static final int IS_PART_TIME = 2;
+    private static final int MAX_HR = 100;
+    private static final int WAGE_PER_HR = 20;
+    private static final int MAX_DAY = 20;
     public static void main(String[] args){
 
-        int empHR = 0 , empWage = 0 ,totalEmpWage = 0 ;
-
-        for(int day = 1 ; day <= 20 ; day ++) {
+        int empHR = 0 , empWage = 0 , totalWorkingHr = 0 ,day = 0 ;
+        System.out.println(IS_FULL_TIME);
+        while(totalWorkingHr < MAX_HR && day < MAX_DAY) {
+            day ++;
             int empCheck = (int) Math.floor((Math.random() * 10)) % 3;
             switch (empCheck) {
                 case IS_FULL_TIME:
@@ -24,10 +26,10 @@ class EmpWageBuilder {
                     empHR = 0;
                     break;
             }
-            empWage = empHR * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println("Employee wage: " + empWage);
+            empWage = empHR + (WAGE_PER_HR * empHR);
+            day ++;
+            totalWorkingHr = totalWorkingHr + empHR;
         }
-        System.out.println("Total Employee wage: " + totalEmpWage);
+        System.out.println("Employee wage: " + empWage);
     }
 }
